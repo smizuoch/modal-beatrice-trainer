67ff600664c442c144f2c7c5dcff6b35598b4f81
smizuoch <shotaro.mizuochi@proton.me>
2025-02-01 17:50:35 +0900

    fix code

--- modal_train.py
+++ modal_train.py
@@ -9,12 +9,11 @@ volume = modal.Volume.from_name("beatrice-models", create_if_missing=True)
 image = (
     modal.Image.from_registry("python:3.10-slim")
     .apt_install("g++", "build-essential")
-    .pip_install("poetry", "torch", "torchaudio", "pyworld", "tensorboard")
+    .pip_install("poetry")
     .add_local_dir(".", remote_path="/workspace")
 )
 
 # Function の volumes 引数で Volume を /workspace/model にマウントする
-# GPU は A100 などに変更可能
 @app.function(image=image, volumes={"/workspace/model": volume}, gpu="A10G", timeout=3600)
 def train(data_dir: str, out_dir: str):
     import subprocess
@@ -22,7 +21,6 @@ def train(data_dir: str, out_dir: str):
         "cd /workspace && "
         "poetry config virtualenvs.create false && "
         "poetry install && "
-        "python3 beatrice_trainer -d {data_dir} -o {out_dir}"
-        # "python3 beatrice_trainer -d {data_dir} -o {out_dir} -c assets/my_config.json" # コンフィグファイルを指定する場合
+        "python3 beatrice_trainer -d {data_dir} -o {out_dir} -c assets/my_config.json"
     ).format(data_dir=data_dir, out_dir=out_dir)
     subprocess.run(cmd, shell=True, check=True)
